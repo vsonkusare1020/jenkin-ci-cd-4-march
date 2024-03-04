20a497ce34a9d4862079c6bbb3d22162e3d77a27
vsonkusare1020 <96275813+vsonkusare1020@users.noreply.github.com>
2024-03-04 18:22:02 +0530

    added file

---
+++ src/main/java/com/vs/jenkincicdfourthmarch/JenkinCiCdFourthMarchApplication.java
@@ -0,0 +1,13 @@
+package com.vs.jenkincicdfourthmarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinCiCdFourthMarchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinCiCdFourthMarchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vs/jenkincicdfourthmarch/JenkinCiCdFourthMarchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vs.jenkincicdfourthmarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinCiCdFourthMarchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
